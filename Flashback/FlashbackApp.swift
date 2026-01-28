ed90d07ef897c6cebdaeaa50ffb376476319230e
Matthew Lu <pamalu.225@gmail.com>
2026-01-27 19:14:45 -0500

    add local code

---
+++ Flashback/FlashbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashbackApp.swift
+//  Flashback
+//
+//  Created by Matthew Lu on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
